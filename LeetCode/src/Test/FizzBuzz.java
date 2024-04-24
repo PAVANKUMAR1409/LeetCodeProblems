5c457be99362b7a219bf01868dfa62bf8ad622fa
# E.-PAVANKUMAR <pavanelagandula@gmail.com>
2024-04-24 12:46:05 +0530

    FizzBuzz IBM 24/04

---
+++ LeetCode/src/Test/FizzBuzz.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		int n=15;
+		for(int i=1;i<=n;i++) {
+			System.out.println((i%5==0 && i%3==0)?"FizzBuzz":(i%5==0)?"Fizz":(i%3==0)?"Buzz":i);
+		}
+	}
+}
